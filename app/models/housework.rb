f0369fcf0b6a8fcc5d8ba20cf7f0abe81f4ecfc1
ranburu1 <gyadas3347@gmail.com>
2021-11-03 15:31:27 +0900

    家事登録ができない時のエラーメッセージ

--- app/models/housework.rb
+++ app/models/housework.rb
@@ -1,4 +1,4 @@
 class Housework < ApplicationRecord
-  validates :title,  presence: true
-  validates :manner, presence: true
+  validates :title,  presence: { message: 'タイトルを入力してください' }
+  validates :manner, presence: { message: 'やり方を入力してください' }
 end
