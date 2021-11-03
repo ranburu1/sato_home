b55a1da644c1f80956cc08e2a8de3b655b0eeb41
ranburu1 <gyadas3347@gmail.com>
2021-11-03 11:48:43 +0900

    new,createアクション設定

--- app/controllers/houseworks_controller.rb
+++ app/controllers/houseworks_controller.rb
@@ -1,8 +1,21 @@
 class HouseworksController < ApplicationController
   def new
+    @housework = Housework.new
   end
-  
+
   def create
+    @housework = Housework.new(housework_params)
+    if @housework.save
+      redirect_to root_path
+    else
+        render :new
+    end
+  end
+
+  private
+
+  def housework_params
+    params.require(:housework).permit(:title, :manner)
   end
 
 end
