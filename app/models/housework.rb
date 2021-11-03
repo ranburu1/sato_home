eb81a80df9f775a4d67f411438baafa9103745c3
ranburu1 <gyadas3347@gmail.com>
2021-11-03 22:44:12 +0900

    serchアクション設定

--- app/models/housework.rb
+++ app/models/housework.rb
@@ -1,4 +1,12 @@
 class Housework < ApplicationRecord
   validates :title,  presence: { message: 'タイトルを入力してください' }
   validates :manner, presence: { message: 'やり方を入力してください' }
+
+  def self.search(search)
+    if search != ""
+      Housework.where('title LIKE(?)', "%#{search}%")
+    else
+      Housework.all
+    end
+  end
 end
